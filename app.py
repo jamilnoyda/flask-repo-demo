87f250d72efe35a493b9201cae069d8c367a8db0
Jamil Noyda <jamil_noyda@gridle.io>
2020-03-02 16:06:31 +0530

    second commit

--- app.py
+++ app.py
@@ -1,43 +1,39 @@
 from flask import Flask
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "you-will-never-guess"
+from flask import render_template, flash, redirect, url_for
+
+
+from routes import *
+
+from flask import render_template
+from flask import request, render_template, jsonify
+from forms import *
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = UserForm()
+    
+    if form.validate_on_submit():
+        
+        return redirect(url_for('index'))
+
+    return render_template(
+        'login.html',
+        title='Sign In',
+        form=form
+    )
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template(
+        'index.html',
+        title='Sign In',
+        # form=form
+    )
 
 
 
-
-
-# import os
-
-# from flask import Flask
-
-
-# def create_app(test_config=None):
-#     # create and configure the app
-#     app = Flask(__name__, instance_relative_config=True)
-#     app.config.from_mapping(
-#         SECRET_KEY='dev',
-#         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-#     )
-
-#     if test_config is None:
-#         # load the instance config, if it exists, when not testing
-#         app.config.from_pyfile('config.py', silent=True)
-#     else:
-#         # load the test config if passed in
-#         app.config.from_mapping(test_config)
-
-#     # ensure the instance folder exists
-#     try:
-#         os.makedirs(app.instance_path)
-#     except OSError:
-#         pass
-
-#     # a simple page that says hello
-#     @app.route('/hello')
-#     def hello():
-#         return 'Hello, World!'
-
+    
 
--- views.py
+++
@@ -1,4 +0,0 @@
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
